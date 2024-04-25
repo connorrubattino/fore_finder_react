88e04af8f744f04edada6248071057b34398ce57
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-25 13:07:21 -0700

    added logo and tightened up search bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,36 +5,45 @@ import Nav from 'react-bootstrap/Nav';
 
 
 type NavigationProps = {
-    isLoggedIn:boolean,
+    isLoggedIn: boolean,
     logUserOut: () => void,
 }
 
-export default function Navigation({isLoggedIn, logUserOut}: NavigationProps){
+export default function Navigation({ isLoggedIn, logUserOut }: NavigationProps) {
 
 
     return (
         <Navbar expand='lg' className="bg-body-success">
             <Container fluid>
-            <Navbar.Brand className='text-white' as={Link} to='/'>Fore Finder</Navbar.Brand>
+                {/* <Navbar.Brand className='text-white' as={Link} to='/'>Fore Finder</Navbar.Brand> */}
+                <Navbar.Brand as={Link} to='/'>
+                    <img
+                        src= '/public/F-ff-logo-design-vector-Graphics-16738354-1-580x386.jpg'
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top ms-3"
+                        alt="Fore Finder Logo"
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='nav-collapse' />
                 <Navbar.Collapse id='nav-collapse'>
                     <Nav className='me-auto'>
-                    {isLoggedIn ? (
-                    <>
-                        <Nav.Link className='text-white' as={Link} to="/teetimes">Find Tee Times</Nav.Link>
-                        {/* need to create route for just seeing own tee times */}
-                        <Nav.Link className='text-white' as={Link} to="/teetimes/me">My Tee Times</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to='/golfers/me'>My Profile</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to='/courses'>Courses & Course IDs</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to='/' onClick={()=> logUserOut()}>Log Out</Nav.Link>
-                    </>
-                    ): (
-                    <>    
-                        <Nav.Link className='text-white' as={Link} to ='/golfers'>Sign Up</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to ='/login'>Log In</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to='/courses'>Courses & Course IDs</Nav.Link>
-                    </>
-                    )}
+                        {isLoggedIn ? (
+                            <>
+                                <Nav.Link className='text-white' as={Link} to="/teetimes">Find Tee Times</Nav.Link>
+                                {/* need to create route for just seeing own tee times */}
+                                <Nav.Link className='text-white' as={Link} to="/teetimes/me">My Tee Times</Nav.Link>
+                                <Nav.Link className='text-white' as={Link} to='/golfers/me'>My Profile</Nav.Link>
+                                <Nav.Link className='text-white' as={Link} to='/courses'>Courses & Course IDs</Nav.Link>
+                                <Nav.Link className='text-white' as={Link} to='/' onClick={() => logUserOut()}>Log Out</Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link className='text-white' as={Link} to='/golfers'>Sign Up</Nav.Link>
+                                <Nav.Link className='text-white' as={Link} to='/login'>Log In</Nav.Link>
+                                <Nav.Link className='text-white' as={Link} to='/courses'>Courses & Course IDs</Nav.Link>
+                            </>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/views/Teetimes.tsx
+++ src/views/Teetimes.tsx
@@ -11,8 +11,8 @@ import { getAllTeetimes, createTeetime } from '../lib/apiWrapper';
 
 type TeetimesProps = {
     isLoggedIn: boolean,
-    currentUser:GolferType|null,
-    flashMessage: (newMessage:string, newCategory:CategoryType) => void
+    currentUser: GolferType | null,
+    flashMessage: (newMessage: string, newCategory: CategoryType) => void
 
 }
 
@@ -26,9 +26,9 @@ export default function Teetimes({ isLoggedIn, currentUser, flashMessage }: Teet
     const [fetchTeetimeData, setFetchTeetimeData] = useState(true);
 
     useEffect(() => {     //function that takes in a function and useEffect() runs after every render
-        async function fetchData(){
+        async function fetchData() {
             const response = await getAllTeetimes();
-            if (response.data){
+            if (response.data) {
                 let teetimes = response.data;
                 // teetimes.sort( (a, b) => (new Date(a.id) > new Date(b.id) ? -1 : 1) )
                 // unsure on sorting - just doing by most recent as of now
@@ -49,9 +49,9 @@ export default function Teetimes({ isLoggedIn, currentUser, flashMessage }: Teet
     const addNewTeetime = async (newTeetimeData: TeetimeFormType) => {
         const token = localStorage.getItem('token') || '';
         const response = await createTeetime(token, newTeetimeData)
-        if (response.error){
+        if (response.error) {
             flashMessage(response.error, 'danger')
-        } else if (response.data){
+        } else if (response.data) {
             flashMessage(`Tee Time at ${response.data.course_name} has been created`, 'success')
             setShowForm(false);
             setFetchTeetimeData(!fetchTeetimeData)
@@ -59,24 +59,23 @@ export default function Teetimes({ isLoggedIn, currentUser, flashMessage }: Teet
     }
 
 
-  return (
-    <>
-        <h1 className="text-center text-white m-5">{isLoggedIn && currentUser ? `Hello ${currentUser?.first_name}!` : 'Welcome to Fore Fidner - Please Login or Sign Up!'}</h1>
-        <Row>
-            <Col xs={12} md={6}>
-                <Form.Control value={searchTerm} placeholder='Search Courses with Tee Times' onChange={handleInputChange}/>
-            </Col>
-            {isLoggedIn && (
-                <Col>
-                    <Button className='w-100' variant='success' onClick={() => setShowForm(!showForm)}>{showForm ? 'Hide Form' : 'Add Tee Time+'}</Button>
+    return (
+        <>
+            <h1 className="text-center text-white m-5">{isLoggedIn && currentUser ? `Hello ${currentUser?.first_name}!` : 'Welcome to Fore Fidner - Please Login or Sign Up!'}</h1>
+            <Row className="justify-content-center">
+                <Col xs={12} md={6}>
+                    <Form.Control className='ms-4 text-center w-100' value={searchTerm} placeholder='Search Courses with Tee Times' onChange={handleInputChange} />
                 </Col>
-            )
-            }
-        </Row>
-        { showForm && <TeetimeForm addNewTeetime={addNewTeetime}/> }
-        <Row className='me-5'>
-        {teetimes.filter(t => t.course_name.toLowerCase().includes(searchTerm.toLowerCase())).map( t => <Col key={t.course_name} xs={6} md={4} lg={3}> <TeetimeCard key={t.course_name} teetime={t} currentUser={currentUser} /></Col> )}
-        </Row>
-    </>
-  )
+                {isLoggedIn && (
+                    <Col>
+                        <Button className='w-100 ms-4' variant='success' onClick={() => setShowForm(!showForm)}>{showForm ? 'Hide Form' : 'Add Tee Time+'}</Button>
+                    </Col>
+                )}
+            </Row>
+            {showForm && <TeetimeForm addNewTeetime={addNewTeetime} />}
+            <Row className='me-5'>
+                {teetimes.filter(t => t.course_name.toLowerCase().includes(searchTerm.toLowerCase())).map(t => <Col key={t.course_name} xs={6} md={4} lg={3}> <TeetimeCard key={t.course_name} teetime={t} currentUser={currentUser} /></Col>)}
+            </Row>
+        </>
+    )
 }
